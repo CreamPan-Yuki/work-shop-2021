80e8705e7589b2b90331c1fae215861484a0bef3
creampan-yuki <snow.yuki.calc@gmail.com>
2021-06-28 20:11:52 +0900

    Server-1
    初期コミット

---
+++ server/src/main/java/com/example/springboot/Screen.java
@@ -0,0 +1,13 @@
+package com.example.springboot;
+
+public class Screen {
+	private String value;
+	
+	Screen() {
+		this.value = "start";
+	}
+	
+	public String getValue() {
+		return this.value;
+	}
+}
